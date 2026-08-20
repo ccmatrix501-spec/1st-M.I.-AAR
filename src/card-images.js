7a9acf4006a01bf0e630e1b7569f4bf20f5b4031
ccmatrix501-spec <ccmatrix501@gmail.com>
2026-08-20 14:51:19 +1000

    Add onboarding card image loader

---
+++ src/card-images.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  'complete.png': require('./cards/complete'),
+  'paused.png': require('./cards/paused'),
+  'step1.png': require('./cards/step1'),
+  'step2.png': require('./cards/step2'),
+  'step3-platform.png': require('./cards/step3-platform'),
+  'step3-rules-ambassador.png': require('./cards/step3-rules-ambassador'),
+  'step3-rules-returning.png': require('./cards/step3-rules-returning'),
+  'step4-community.png': require('./cards/step4-community'),
+  'step4-experience.png': require('./cards/step4-experience'),
+  'step4-name.png': require('./cards/step4-name'),
+  'step4-rank.png': require('./cards/step4-rank'),
+};
